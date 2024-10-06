df6a202fa0ae7c873e7556ead0650c62b8308a56
Vi-deve31 <qveentoria@gmail.com>
2024-10-06 14:21:59 -0400

    added configuration for mongodb added additional connections for mongodb in applications.properties

---
+++ GeekText/src/main/java/com/example/GeekText/configuration/MongoDBConfig.java
@@ -0,0 +1,24 @@
+package com.example.GeekText.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+@EnableMongoRepositories(basePackages="com.example.GeekText.repository")
+public class MongoDBConfig {
+    
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create("mongodb://localhost:27017");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "geektext");
+    }
+}
--- GeekText/src/main/java/com/example/GeekText/model/Book.java
+++ GeekText/src/main/java/com/example/GeekText/model/Book.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "books")
 public class Book {
+    
     @Id
     private String id; 
     private String title;
