4edf1695a2a37e4780ef5dd35d8d8f622fdac870
ksanchh <kevinsanchez213@yahoo.com>
2024-10-22 21:39:00 -0400

    implemented GET bookDetails by ID

--- GeekText/src/main/java/com/example/GeekText/Controller/MainController.java
+++ GeekText/src/main/java/com/example/GeekText/Controller/MainController.java
@@ -3,9 +3,7 @@ package com.example.GeekText.Controller;
 import com.example.GeekText.Model.BookDetails;
 import com.example.GeekText.Repository.BookDetailsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
@@ -18,4 +16,12 @@ public class MainController {
     public void addBook(@RequestBody BookDetails bookDetails) {
         bookDetailsRepo.save(bookDetails);
     }
+
+
+    @GetMapping("/getBook/{id}")
+    public BookDetails getBook(@PathVariable Integer id) {
+        return bookDetailsRepo.findById(id).orElse(null);
+
+    }
+
 }
